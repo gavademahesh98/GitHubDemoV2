99dbcc80a3013a9e3629537518db42db48bee7ea
vikram <vikram@DESKTOP-E0HGB7H>
2022-06-15 18:59:14 +0530

    i have added int b = 200 ;

--- src/demo/ClassFour.java
+++ src/demo/ClassFour.java
@@ -4,7 +4,8 @@ public class ClassFour {
 	
 	public static void main (String[]args) {
 		int a = 100 ; 
-		System.out.println(a + "Mahesh DADA");
+		int b = 200 ;
+		System.out.println(a+b + " Mahesh DADA");
 	}
 
 }
