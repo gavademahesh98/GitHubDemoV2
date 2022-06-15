3a7409ee51c299f4d707b813351e453858c46c50
MAHESH <MAHESH@HP-Pavilion>
2022-06-15 09:30:27 +0530

    First Commit

---
+++ src/demo/Condition.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Condition {
+	public static void main(String[] args) {
+		String s1="Mahesh";
+		if(s1=="Mahesh")
+		{
+			System.out.println("Good Boy");
+		}
+		else {
+		System.out.println("Bad Boy");
+		}
+	}
+
+}
